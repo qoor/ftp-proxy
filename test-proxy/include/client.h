ee9c499551003eebede88d91b76192dd09219c4c
onetop4458 <brent@lsware.co.kr>
2020-01-21 18:23:11 +0900

    mod : fix client

--- test-proxy/include/client.h
+++ test-proxy/include/client.h
@@ -1,6 +1,7 @@
 #ifndef __CLIENT_H__
 #define __CLIENT_H__
 
-#define MAX_EVENTS 254
+#define MAX_CLIENT_EVENTS 254
+#define BIND_CLIENT_PORT 7777
 void epoll(void); /* 임시 함수임 수정 예정 */
 #endif
--- test-proxy/include/log.h
+++ test-proxy/include/log.h
@@ -3,8 +3,8 @@
 
 #define DEFAULT_LOG_FILE_NAME	"proxy"
 #define DEFAULT_LOG_FILE_EXT	"log"
-#define DEFAULT_LOG_FILE_PATH 	("" DEFAULT_LOG_FILE_NAME "." DEFAULT_LOG_FILE_EXT)
-#define MAX_LOG_MESSAGE_SIZE	(1024)
+#define DEFAULT_LOG_FILE_PATH 	DEFAULT_LOG_FILE_NAME "." DEFAULT_LOG_FILE_EXT
+#define MAX_LOG_MESSAGE_SIZE	1024
 
 int log_init(void);
 int log_write(const char* message, ...);
