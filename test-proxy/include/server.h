87442c983dfef06fc0a65135f9fd6c12d23cc4fc
Qoo <akck1202@hanmail.net>
2020-01-29 17:31:39 +0900

    Reimplementing server

--- test-proxy/include/server.h
+++ test-proxy/include/server.h
@@ -5,7 +5,7 @@
 #include <netinet/ip.h>
 
 #include "vector.h"
-#include "hashmap.h"
+#include "session.h"
 
 #define MAX_EVENTS (254) /* Amount of file descriptor of monitoring */
 #define EVENT_TIMEOUT (0) /* EPOLL event timeout as milliseconds */
@@ -16,43 +16,27 @@
 /* Server info structure */
 struct server
 {
-	int socket_fd;
-	int epoll_fd;
-	struct sockaddr_in* socket_address;
+	int socket[MAX_SESSION_SOCKETS];
+	struct sockaddr_in socket_address;
 };
 /* */
 
 /* SERVER RETURN CODE DEFINE */
-enum server_add_error_type
+enum server_error_type
 {
-	SERVER_ADD_SUCCESS,
-	SERVER_ADD_ALLOC_FAILED,
-	SERVER_ADD_INCORRECT_CONNECTION_STRING,
-	SERVER_ADD_SOCKET_CREATE_FAILED,
-	SERVER_ADD_NO_SERVERS
-};
-
-enum server_remove_error_type
-{
-	SERVER_REMOVE_SUCCESS,
-	SERVER_REMOVE_INVALID_SERVER
-};
-
-enum servers_polling_error_type
-{
-	SERVERS_POLLING_SUCCESS,
-	SERVERS_POLLING_WAIT_ERROR
+	SERVER_SUCCESS,
+	SERVER_INVALID,
+	SERVER_ALLOC_FAILED,
+	SERVER_INCORRECT_CONNECTION_STRING,
+	SERVER_SOCKET_CREATE_FAILED,
+	SERVER_NO_SERVERS,
+	SERVER_POLLING_WAIT_ERROR
 };
 /* */
 
-int add_servers_from_vector(struct hashmap* dest, const struct vector* connection_strings, int epoll_fd);
-int add_server(struct hashmap* dest, const char* connection_string, int epoll_fd);
-void reset_server_list(struct hashmap* dest);
-void server_packet_received(const struct server* server, unsigned char* packet);
-void send_packet_to_server(struct server* dst_server, unsigned char* packet);
-int servers_polling(int epoll_fd, const struct hashmap* server_list, struct epoll_event** events);
-struct server* get_server_from_fd(const struct hashmap* server_list, int fd);
-int get_packet_from_server(const struct server* server, struct iphdr* ip_header, unsigned char** packet);
+int server_remove_from_list(struct vector* server_list, const struct server* target_server);
+int server_remove_from_list_index(struct vector* server_list, int index);
+int server_register_servers_to_epoll(struct vector* server_list, int target_epoll_fd);
 
 #endif
 
--- test-proxy/src/server.c
+++ test-proxy/src/server.c
@@ -2,280 +2,205 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
 #include <malloc.h>
 #include <string.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <unistd.h>
 
-#include <netinet/ip.h>
-#include <netinet/in.h>
-
-#include "packet.h"
-#include "types.h"
-
-static void server_free(struct server* target_server);
-
-static int remove_server_loop(void* key, void* value, void* context)
+static void server_set_socket_option(int socket_fd)
 {
-	server_free((struct server*)value);
-	return 1;
-}
-
-/* Add server to destination from connection strings */
-int add_servers_from_vector(struct hashmap* dest, const struct vector* connection_strings, int epoll_fd)
-{
-	int i = 0;
-	int server_count = 0;
-	char* current_string = NULL;
-	int ret = SERVER_ADD_SUCCESS;
-
-	if (connection_strings == NULL)
-	{
-		return SERVER_ADD_INCORRECT_CONNECTION_STRING;
-	}
-
-	server_count = connection_strings->size;
-
-	if (server_count == 0)
-	{
-		return SERVER_ADD_NO_SERVERS;
-	}
-
-	for (i = 0; i < server_count; ++i)
-	{
-		if ((current_string = connection_strings->container[i]) == NULL)
-		{
-			continue;
-		}
-
-		ret = add_server(dest, current_string, epoll_fd);
-		if (ret != SERVER_ADD_SUCCESS)
-		{
-			break;
-		}
-	}
-
-	return ret;
-}
-
-int add_server(struct hashmap* dest, const char* connection_string, int epoll_fd)
-{
-	struct server* server_object = NULL;
-	char* colon_pos = NULL;
-	char server_ip[32];
-	size_t address_length = 0;
-	int header_include = TRUE;
 	int flags = 0;
-	struct epoll_event epoll_event;
 
-	memset(&epoll_event, 0x00, sizeof(struct epoll_event));
-	epoll_event.events = EPOLLIN;
-
-	if (dest == NULL)
-	{
-		return SERVER_ADD_ALLOC_FAILED;
-	}
-	if (connection_string == NULL)
-	{
-		return SERVER_ADD_INCORRECT_CONNECTION_STRING;
-	}
-
-	server_object = (struct server*)malloc(sizeof(struct server));
-	if (server_object == NULL)
-	{
-		return SERVER_ADD_ALLOC_FAILED;
-	}
-
-	server_object->socket_fd = -1;
-	server_object->socket_address = (struct sockaddr_in*)malloc(sizeof(struct sockaddr_in));
-	if (server_object->socket_address == NULL)
-	{
-		server_free(server_object);
-
-		return SERVER_ADD_ALLOC_FAILED;
-	}
-
-	memset(server_object->socket_address, 0x00, sizeof(struct sockaddr_in));
-
-	colon_pos = strchr(connection_string, ':');
-	if (colon_pos == NULL)
-	{
-		server_free(server_object);
-
-		return SERVER_ADD_INCORRECT_CONNECTION_STRING;
-	}
-
-	address_length = colon_pos - connection_string;
-	strncpy(server_ip, connection_string, address_length + 1);
-	server_ip[address_length] = '\0';
-	server_object->socket_address->sin_addr.s_addr = htonl(atoi(server_ip));
-	server_object->socket_address->sin_port = htons(atoi(colon_pos + 1));
-
-	server_object->socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
-	if (server_object->socket_fd == -1)
-	{
-		server_free(server_object);
-
-		return SERVER_ADD_SOCKET_CREATE_FAILED;
-	}
-	if (setsockopt(server_object->socket_fd, IPPROTO_IP, IP_HDRINCL, &header_include, sizeof(header_include)) == -1 ||
-		epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_object->socket_fd, &epoll_event))
-	{
-		server_free(server_object);
-
-		return SERVER_ADD_SOCKET_CREATE_FAILED;
-	}
-
-	flags = fcntl(server_object->socket_fd, F_GETFL, 0);
-	fcntl(server_object->socket_fd, F_SETFL, flags | O_NONBLOCK);
-
-	hashmap_insert(dest, &server_object->socket_fd, server_object);
-	if (errno != HASHMAP_SUCCESS)
-	{
-		server_free(server_object);
-
-		return SERVER_ADD_ALLOC_FAILED;
-	}
-
-	return SERVER_ADD_SUCCESS;
+	flags = fcntl(socket_fd, F_GETFL);
+	fcntl(socket_fd, F_SETFL, flags & O_NONBLOCK);
 }
 
-static void server_free(struct server* target_server)
+static int server_free(struct server* target_server)
 {
-	if (target_server->socket_fd != -1)
+	int socket_fd = -1;
+	int i = 0;
+
+	if (target_server == NULL)
 	{
-		close(target_server->socket_fd);
-		target_server->socket_fd = -1;
+		return SERVER_INVALID;
 	}
 
-	if (target_server->socket_address != NULL)
+	for ( ; i < MAX_SESSION_SOCKETS; ++i)
 	{
-		free(target_server->socket_address);
-		target_server->socket_address = NULL;
+		socket_fd = target_server->socket[i];
+		if (socket_fd != -1)
+		{
+			shutdown(socket_fd, SHUT_RDWR);
+			close(socket_fd);
+		}
 	}
 
 	free(target_server);
+
+	return SERVER_SUCCESS;
 }
 
-void reset_server_list(struct hashmap* dest)
+static struct server* server_create()
 {
-	hashmap_foreach(dest, remove_server_loop, NULL);
-	hashmap_clear(dest);
+	struct server* new_server = NULL;
+	int new_socket = -1;
+	int i = 0;
+
+	new_server = (struct server*)malloc(sizeof(struct server));
+	if (new_server == NULL)
+	{
+		return NULL;
+	}
+
+	/* Must reset socket before `server_free` */
+	for (i = 0; i < MAX_SESSION_SOCKETS; ++i)
+	{
+		new_server->socket[i] = -1;
+	}
+
+	for (i = 0; i < MAX_SESSION_SOCKETS; ++i)
+	{
+		new_server->socket[i] = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+		new_socket = new_server->socket[i];
+		if (new_socket < 0)
+		{
+			server_free(new_server);
+			return NULL;
+		}	
+	}
+
+	return new_server;
 }
 
-/* 
- * An handler of Packet received from server
- * This function role full valid packet received event alert to client
-*/
-void server_packet_received(const struct server* server, unsigned char* packet)
+static int server_connect(int socket_fd, uint32_t address, uint16_t port)
 {
-	/* Require packet receive handling client.c */
-	/*struct iphdr* ip_header = (struct iphdr*)packet;
-	struct client* dest_client = get_client_from_address(ip_header->daddr);
+	struct sockaddr_in socket_address = { 0, };
+	int ret = 0;
 
-	client_packet_receive_handler(dest_client, server, packet);
-	*/
+	socket_address.sin_addr.s_addr = htonl(address);
+	socket_address.sin_port = htons(port);
+	socket_address.sin_family = AF_INET;
+
+	server_set_socket_option(socket_fd);
+	ret = connect(socket_fd, (struct sockaddr*)&socket_address, sizeof(struct sockaddr));
+	if (ret < 0 && errno != EINPROGRESS)
+	{
+		return SERVER_SOCKET_CREATE_FAILED;
+	}
+
+	return SERVER_SUCCESS;
 }
 
-void send_packet_to_server(struct server* dst_server, unsigned char* packet)
+static int server_listen(int socket_fd, uint16_t port)
 {
-	change_packet_address(packet, dst_server->socket_address, PACKET_ADDR_CHANGE_TYPE_DEST);
-	sendto(dst_server->socket_fd, packet, ((struct iphdr*)packet)->tot_len, 0x0, (struct sockaddr*)dst_server->socket_address, sizeof(struct sockaddr));
+	struct sockaddr_in bind_socket = { 0, };
+	int ret = 0;
+
+	bind_socket.sin_addr.s_addr = htonl(INADDR_ANY);
+	bind_socket.sin_family = AF_INET;
+	bind_socket.sin_port = htons(port);
+	ret = bind(socket_fd, (struct sockaddr*)&bind_socket, sizeof(struct sockaddr));
+	if (ret == -1)
+	{
+		return SERVER_SOCKET_CREATE_FAILED;
+	}
+
+	ret = listen(socket_fd, SOMAXCONN);
+	if (ret == -1)
+	{
+		return SERVER_SOCKET_CREATE_FAILED;
+	}
+
+	return SERVER_SUCCESS;
 }
 
-int servers_polling(int epoll_fd, const struct hashmap* server_list, struct epoll_event** events)
+static int server_add_to_list(struct vector* server_list, uint32_t address, struct server** created_server)
 {
-	int active_events = 0;
-	int eventid = 0;
-	struct server* server_ptr = NULL;
-	static struct iphdr ip_header = { 0, };
-	unsigned char* packet = NULL;
+	struct server* new_server = NULL;
+	int ret = -1;
 
 	errno = 0;
 
-	active_events = epoll_wait(epoll_fd, *events, MAX_EVENTS, EVENT_TIMEOUT);
-	if (active_events == -1 && errno != EINTR)
+	if (created_server == NULL)
 	{
-		return SERVERS_POLLING_WAIT_ERROR;
+		errno = SERVER_INVALID;
+		return -1;
 	}
 
-	for (eventid = 0; eventid < active_events; ++eventid)
+	*created_server = NULL;
+
+	if (server_list == NULL)
 	{
-		server_ptr = (struct server*)hashmap_get(server_list, &events[eventid]->data.fd);
-		memset(&ip_header, 0x00, sizeof(struct iphdr));
-
-		if (server_ptr == NULL)
-		{
-			/* Invalid socket descriptor */
-			continue;
-		}
-
-		if (get_packet_from_server(server_ptr, &ip_header, &packet) != PACKET_SUCCESS)
-		{
-			/* Not valid packet */
-			continue;
-		}
-
-		server_packet_received(server_ptr, packet);
-		free(packet);
-		packet = NULL;
+		errno = SERVER_INVALID;
+		return -1;
 	}
 
-	return SERVERS_POLLING_SUCCESS;
+	new_server = server_create();
+	if (new_server == NULL)
+	{
+		errno = SERVER_ALLOC_FAILED;
+		return -1;
+	}
+
+	ret = server_connect(new_server->socket[PORT_TYPE_COMMAND], address, FTP_COMMAND_PORT);
+	if (ret != SERVER_SUCCESS)
+	{
+		server_free(new_server);
+		errno = SERVER_SOCKET_CREATE_FAILED;
+		return -1;
+	}
+
+	ret = server_listen(new_server->socket[PORT_TYPE_DATA], FTP_DATA_PORT);
+	if (ret != SERVER_SUCCESS)
+	{
+		server_free(new_server);
+		errno = SERVER_SOCKET_CREATE_FAILED;
+		return -1;
+	}
+
+	if (vector_push_back(server_list, new_server) != VECTOR_SUCCESS)
+	{
+		server_free(new_server);
+		errno = SERVER_ALLOC_FAILED;
+		return -1;
+	}
+
+	*created_server = new_server;
+
+	return (server_list->size - 1);
 }
 
-/*
- * Filtering packet from all of server packets
- * This function role filtering FTP (data/command) packet from all of server source packets
-*/ 
-int get_packet_from_server(const struct server* server, struct iphdr* ip_header, unsigned char** packet)
-{	
-	struct tcphdr* tcp_header = NULL;
+int server_remove_from_list(struct vector* server_list, const struct server* target_server)
+{
+	int i = 0;
 
-	if (recv(server->socket_fd, ip_header, 20, 0) < 20 || ip_header->tot_len < 40)
+	if (server_list == NULL || target_server == NULL)
 	{
-		/* Invalid packet */
-		return PACKET_INVALID;
-	}
-	if (ip_header->protocol != IPPROTO_TCP)
-	{
-		return PACKET_INVALID;
+		return SERVER_INVALID;
 	}
 
-	*packet = (unsigned char*)malloc(ip_header->tot_len);
-	if (*packet == NULL)
+	for ( ; i < server_list->size; ++i)
 	{
-		/* Packet memory allocation failed */
-		return PACKET_ALLOC_FAILED;
+		if (memcmp(target_server, server_list->container[i], sizeof(struct server)) == 0)
+		{
+			return server_remove_from_list_index(server_list, i);
+		}
 	}
 
-	memcpy((char*)*packet, (char*)&ip_header, sizeof(char));
-	if (recv(server->socket_fd, *packet + 20, 20, 0) < 20)
-	{
-		/* Failed to try receiving TCP Header */
-		free(*packet);
+	return SERVER_INVALID;
+}
 
-		return PACKET_INVALID;
+int server_remove_from_list_index(struct vector* server_list, int index)
+{
+	if (server_list == NULL || index < 0 || server_list->size >= index)
+	{
+		return SERVER_INVALID;
 	}
 
-	tcp_header = (struct tcphdr*)*packet + 20;
-	if (tcp_header->source != FTP_COMMAND_PORT && tcp_header->source != FTP_DATA_PORT)
-	{
-		/* Is not a FTP packet */
-		free(*packet);
+	server_free((struct server*)server_list->container[index]);
+	vector_erase(server_list, index);
 
-		return PACKET_INVALID;
-	}
-
-	if (recv(server->socket_fd, *packet + 40, ip_header->tot_len - 40, 0) < ip_header->tot_len - 40)
-	{
-		/* Failed to trying to get packet data */
-		free(*packet);
-
-		return PACKET_INVALID;
-	}
-
-	return PACKET_SUCCESS;
+	return SERVER_SUCCESS;
 }
 
